9eb9d3decd2c61d82c464670d6df16e37e6cd9a4
namvu <vuducnama6@example.com>
2024-02-16 22:05:53 +0700

    update 16/2

--- src/app/controllers/allProductsController.js
+++ src/app/controllers/allProductsController.js
@@ -9,23 +9,19 @@ class allProductsController {
 
   showFlashSale(req, res, next) {
     product.find({ deletedAt: null, hotsale: 'flash-sale' }).lean().sortable(req)
-      .then(product => { 
-        res.render('users/allProductsFlashSale', { title: 'Sản phẩm Flash Saleeeee', product }) })
+      .then(product => { res.render('users/allProductsFlashSale', { title: 'Sản phẩm Flash Saleeeee', product }) })
       .catch(next)
   }
 
   showHot(req, res, next) {
     product.find({ deletedAt: null, hotsale: 'hot' }).lean().sortable(req)
-      .then(product => { 
-        res.render('users/allProductsHot', { title: 'Sản phẩm Hottttt', product }) })
+      .then(product => { res.render('users/allProductsHot', { title: 'Sản phẩm Hottttt', product }) })
       .catch(next)
   }
 
   showCategory(req, res, next) {
-    product.find({ deletedAt: null, skincare: req.params.slug }).lean().sortable(req)
-      .then(product => { 
-        // const categoryProduct = product.filter(product => product.skincare || product.skincare === 'hot')
-        res.render('users/allProductsHot', { title: 'Sản phẩm Hottttt', product }) })
+    product.find({ deletedAt: null, $or: [{skincare: req.params.slug}, {makeup: req.params.slug}] }).lean().sortable(req)
+      .then(product => { res.render('users/allProductsHot', { title: req.params.slug, product }) })
       .catch(next)
   }
 }
