012edf89fd2581bce0b39134cade77f726295f85
namvu <vuducnama6@example.com>
2024-08-26 17:01:43 +0700

    update 26/8

--- src/app/controllers/homeController.js
+++ src/app/controllers/homeController.js
@@ -5,17 +5,8 @@ class homeController {
     product.find({ deletedAt: null }).lean()
       .then(product => {
         const flashDealProduct    = product.filter(product => product.status === 'flash-sale').slice(0, 5)
-        flashDealProduct.price    = flashDealProduct.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-        flashDealProduct.oldPrice = flashDealProduct.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-        
         const hotProduct    = product.filter(product => product.status === 'hot').slice(0, 5)
-        hotProduct.price    = hotProduct.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-        hotProduct.oldPrice = hotProduct.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-        
         const allProduct    = product.slice(0, 5)
-        allProduct.price    = allProduct.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-        allProduct.oldPrice = allProduct.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-
         const allBrands = [...new Set(product.map(product => product.brand))]
 
         res.render('users/home', { title: 'Cửa hàng mỹ phẩm BunnyStore' , flashDealProduct, hotProduct, allProduct, allBrands }) })
