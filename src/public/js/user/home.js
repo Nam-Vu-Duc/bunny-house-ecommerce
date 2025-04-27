bfc3706a5905ff24046a3dc3d1ac0211bcbe39fd
namvu <vuducnama6@gmail.com>
2025-04-27 23:06:33 +0700

    feat: update new part for the home page

--- src/public/js/user/home.js
+++ src/public/js/user/home.js
@@ -2,6 +2,7 @@
 importLinkCss('/css/user/home.css')
 importLinkCss('/css/user/advertise.css')
 
+const favProductsDiv        = document.querySelector('div[class="products-board"][id="favorite"]').querySelectorAll('div.product')
 const flashSaleProductsDiv  = document.querySelector('div[class="flash-deal-board"][id="flash-deal"]').querySelectorAll('div.product')
 const topSaleProductsDiv    = document.querySelector('div[class="products-board"][id="top-sale"]').querySelectorAll('div.product')
 const hotSaleProductsDiv    = document.querySelector('div[class="products-board"][id="hot-sale"]').querySelectorAll('div.product')
@@ -9,6 +10,32 @@ const skincareProductsDiv   = document.querySelector('div[class="products-board"
 const makeupProductsDiv     = document.querySelector('div[class="products-board"][id="makeup"]').querySelectorAll('div.product')
 const allProductsDiv        = document.querySelector('div[class="products-board"][id="all"]').querySelectorAll('div.product')
 const allBrandsDiv          = document.querySelector('div[class="famous-brand-board"][id="brand"]').querySelectorAll('img')
+
+async function getFavProducts(products) {
+  const response = await fetch('http://localhost:8000/return_data', {
+    method: 'POST',
+    headers: {'Content-Type': 'application/json'},
+  })
+  if (!response.ok) throw new Error(`Response status: ${response.status}`)
+  const {data} = await response.json()
+
+  window.setTimeout(function() {
+    products.forEach((product, index) => {
+      product.querySelector('img').setAttribute('src', data[index].img.path)
+      product.querySelector('img').setAttribute('alt', data[index].img.name)
+      product.querySelector('p#old-price').textContent = formatNumber(data[index].oldPrice) 
+      product.querySelector('p#price').textContent = formatNumber(data[index].price) 
+      product.querySelector('p#name').textContent = data[index].name
+      product.querySelector('span#rate-score').textContent = formatRate(data[index].rate) 
+      product.querySelector('p#sale-number').textContent =  'Đã bán: ' + data[index].saleNumber
+      product.querySelector('div.loading').style.display = 'none'
+      product.querySelectorAll('i').forEach((star, i) => {
+        if (i + 1 <= Math.floor(parseInt(product.querySelector('span#rate-score').innerText))) star.style.color = 'orange'
+      })
+      product.parentElement.setAttribute('href', '/all-products/product/' + data[index]._id)
+    })
+  }, 1000)
+}
 
 async function getProducts(products, filter) {
   const response = await fetch('/data/products', {
@@ -54,22 +81,25 @@ async function getBrands(imgs) {
 
 window.addEventListener('DOMContentLoaded', async function loadData() {
   try {
+    await getFavProducts(favProductsDiv)
+    await new Promise(r => setTimeout(r, 500))
+
     await getProducts(flashSaleProductsDiv, {deletedAt: null, status: 'flash-sale'})
     await new Promise(r => setTimeout(r, 500))
 
-    await getProducts(topSaleProductsDiv  , {deletedAt: null})
+    await getProducts(topSaleProductsDiv, {deletedAt: null})
     await new Promise(r => setTimeout(r, 500))
 
-    await getProducts(hotSaleProductsDiv  , {deletedAt: null, status: 'hot'})
+    await getProducts(hotSaleProductsDiv, {deletedAt: null, status: 'hot'})
     await new Promise(r => setTimeout(r, 500))
 
-    await getProducts(skincareProductsDiv , {deletedAt: null, categories: 'skincare'})
+    await getProducts(skincareProductsDiv, {deletedAt: null, categories: 'skincare'})
     await new Promise(r => setTimeout(r, 500))
 
-    await getProducts(makeupProductsDiv   , {deletedAt: null, categories: 'makeup'})
+    await getProducts(makeupProductsDiv, {deletedAt: null, categories: 'makeup'})
     await new Promise(r => setTimeout(r, 500))
 
-    await getProducts(allProductsDiv      , {deletedAt: null})
+    await getProducts(allProductsDiv, {deletedAt: null})
     await new Promise(r => setTimeout(r, 500))
 
     await getBrands(allBrandsDiv)
