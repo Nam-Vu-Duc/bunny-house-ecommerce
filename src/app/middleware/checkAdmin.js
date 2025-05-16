c7c735c788be6f48fe7e02ad45c90e4b194436d4
namvu <vuducnama6@gmail.com>
2025-05-17 00:07:44 +0700

    fix: solution1-add cache mongodb connect

--- src/app/middleware/checkAdmin.js
+++ src/app/middleware/checkAdmin.js
@@ -1,5 +1,6 @@
 const emp = require('../models/employeeModel')
 const jwt = require('jsonwebtoken')
+const dbConnect = require('../middleware/mongoose')
 
 module.exports = async function checkAdmin(req, res, next) {
   try {
@@ -13,6 +14,7 @@ module.exports = async function checkAdmin(req, res, next) {
       const empInfo = await emp.findOne({ _id: uid })
       if (!empInfo) throw new Error('error')
       if (!['admin', 'manager', 'employee'].includes(empInfo.role)) throw new Error('error')
+      await dbConnect()
       next()
     } else {
       throw new Error('error')
